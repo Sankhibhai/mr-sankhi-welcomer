4cf99930864db562db3bbb053dd65c2a51e765eb
Sankhibhai <bhatiaojas004@gmail.com>
2025-06-08 09:26:03 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -1,11 +1,9 @@
-// shop.js - XP Shop Items (Only Nickname Colors)
-
 const shopItems = [
   {
     id: "nicknamecolor",
     name: "Nickname Color (Red, Blue, Green)",
     price: 1000,
-    description: "Apne nickname ka color badlo. Red, Blue, ya Green me se choose karo purchase ke liye "!buy nicknamecolour red/green,blue"!",
+    description: 'Apne nickname ka color badlo. Red, Blue, ya Green me se choose karo. Purchase command: !buy nicknamecolor <red|blue|green>',
   }
 ];
 
