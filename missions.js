65fca1436f6be8378483b3bef3fa572367359950
Sankhibhai <bhatiaojas004@gmail.com>
2025-06-08 09:01:50 +0530

    Update missions.js

--- missions.js
+++ missions.js
@@ -1,21 +1,22 @@
-// missions.js
-
-const missions = {
-  dailyChat: {
+const missions = [
+  {
+    id: "dailyChat",
     description: "Chat 50 messages in a day",
     xpReward: 200,
     active: true,
   },
-  invite3: {
+  {
+    id: "invite3",
     description: "Invite 3 new members",
     xpReward: 300,
     active: true,
   },
-  voice60: {
+  {
+    id: "voice60",
     description: "Spend 60 minutes in voice channels",
     xpReward: 250,
     active: true,
   },
-};
+];
 
 module.exports = missions;
